acfe8d92d4999d646dd3042ab562114f59da0b30
amanrcbfan99 <amanrcbfan99@gmail.com>
2026-09-15 23:27:53 +0530

    Build a code to find last digit of a number entered by user

--- index.c
+++ index.c
@@ -1,15 +1,10 @@
 # include <stdio.h>
 int main() {
 
-    int number ;
+    int number;
     printf("Enter the number : ");
     scanf("%d", &number);
-    if(number%2==0){
-        printf("Number is Even");
-    } else{
-        printf("Number is odd");
-    }
-
+    printf("%d", number%10);
 
 return 0;
 }
