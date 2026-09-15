45854e9d7076ef60b18416b96ab7575b613efdfe
amanrcbfan99 <amanrcbfan99@gmail.com>
2026-09-15 23:58:59 +0530

    Build a code through which user can determine whether tens digit is larger or unit digit is larger

--- index.c
+++ index.c
@@ -4,7 +4,15 @@ int main() {
     int number;
     printf("Enter three digit number : ");
     scanf("%d", &number);
-    printf("%d", number%100);
+    if((number/10) > (number%10)){
+        printf("Tens digit is greater");
+    } else if ((number/10) == (number%10))
+    {
+        printf("Both are same");
+    }
+    else {
+        printf("Unit digit is greater");
+    }
     
 
 
